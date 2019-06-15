7bd07b73dfe7572113b0a23f406de5b913e0d69e
lwkienun <lwkienun@126.com>
2019-06-15 17:59:42 +0800

    删掉调试代码

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from tesseract_ocr import OCR
 import uuid
 
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+from bottle import error
+@error(404)
+def error404(error):
+    return 'Nothing here, sorry'
+
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
@@ -17,25 +19,6 @@ def server_static(filepath):
 @route('/')
 def root():
     redirect('/static/html/index.html')
-
-url_baidu = "http://fanyi.baidu.com/v2transapi"
-def translate_baidu(content, sign, token, f = "vie", t = "zh"):
-	query = "query=" + urllib.quote(content) + "&from=" + f + "&to=" + t + \
-		"&transtype=translang&simple_means_flag=3&sign="+sign+"&token="+token
-	req = urllib2.Request(url_baidu, query)
-	resp = urllib2.urlopen(req)
-	content = resp.read()
-
-	data = json.loads(content)
-	result = data["trans_result"]["data"][0]["dst"]
-	return result
-
-@route("/translate", method = 'POST')
-def do_translate():
-    content = bottle.request.POST.get('content')
-    sign = bottle.request.POST.get('sign')
-    token = bottle.request.POST.get('token')
-    return translate_baidu(content, sign, token)
 
 #定义上传路径
 save_path = './static/upload/'
@@ -49,11 +32,5 @@ def do_upload():
     upload.raw_filename = savefilename
     upload.save(save_path,overwrite=True)  #把文件保存到save_path路径下
     return OCR(save_path + savefilename)
-
-from bottle import error
-@error(404)
-def error404(error):
-    return 'Nothing here, sorry'
-
 
 run(host='0.0.0.0', port=8080)
